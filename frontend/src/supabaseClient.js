d1de9e927ad84f310428ae961ddfc7cb321112cb
Jefferson Wu <jeffersonw888@gmail.com>
2025-09-08 15:53:54 -0400

    fix supabase client

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xmjsvrukngnwzqwxzfau.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtanN2cnVrbmdud3pxd3h6ZmF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NjY1MzUsImV4cCI6MjA2MDE0MjUzNX0.ZTIwqbRW6pMFkw3MTv4M7tvyOjl8ggoPKuYeFfnSjrk'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
